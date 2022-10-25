ac59f1b2cae54d748124c6aec7b33aee39dd1c11
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-25 16:43:16 -0600

    MS - Simulator.java : Added a simulator class that controls all of the simulation

---
+++ MoneySimulation/src/Simulator.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Simulator {
+
+    private Accounts accounts;
+    private int exchanges;
+
+    public Simulator(int participants, double startingBalance, int exchanges) {
+        this.accounts = new Accounts(participants, startingBalance);
+        this.exchanges = exchanges;
+    }
+
+    public void startSimulation() {
+        System.out.println("************ Pre-simulation Report ************");
+        accounts.printAllAccounts();
+        this.exchangeMoney();
+        System.out.println("************ Post-simulation Report ************");
+        accounts.printAllAccounts();
+    }
+
+    private void exchangeMoney() {
+        for (int i = 0; i < exchanges; i++) {
+
+            Account one = accounts.getRandomAccount();
+            Account two = accounts.getRandomAccount();
+            double exchangeAmount = getExchangeAmount(one, two);
+
+            if (ThreadLocalRandom.current().nextBoolean()) {
+                one.withdraw(exchangeAmount);
+                two.deposit(exchangeAmount);
+            } else {
+                one.deposit(exchangeAmount);
+                two.withdraw(exchangeAmount);
+            }
+        }
+    }
+
+    private double getExchangeAmount(Account one, Account two) {
+        if (one.getBalance() >= two.getBalance()) {
+            return ThreadLocalRandom.current().nextDouble(0,one.getBalance());
+        } else {
+            return ThreadLocalRandom.current().nextDouble(0,two.getBalance());
+        }
+    }
+
+
+
+}
