307ce99ec966fc12035b5e1a6055e5623a462101
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-25 17:54:28 -0600

    MS - Main.java : Added user input to and implemented the program in main

--- MoneySimulation/src/Main.java
+++ MoneySimulation/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter how many people will participate in this simulation: ");
+        int participants = scanner.nextInt();
+        System.out.print("Enter the starting balance of everyone's bank account (ex. 10.20): ");
+        double startingBalance = scanner.nextDouble();
+        System.out.print("Enter how many exchanges will be done in this simulation: ");
+        int exchanges = scanner.nextInt();
+
+        Simulator simulation = new Simulator(participants, startingBalance, exchanges);
+        simulation.startSimulationWithTextFile();
 
     }
 }
