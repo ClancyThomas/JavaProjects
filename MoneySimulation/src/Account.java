06a216d4b8243f003f04668c42aaa0cac6a55d17
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-25 12:50:08 -0600

    MS - Account.java : Added methods to an account

--- MoneySimulation/src/Account.java
+++ MoneySimulation/src/Account.java
@@ -1,4 +1,5 @@
 public class Account {
+
     private double balance;
 
     public Account() {
@@ -8,4 +9,22 @@ public class Account {
     public double getBalance() {
         return balance;
     }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public String toString() {
+        return "$"+balance;
+    }
+    
 }
